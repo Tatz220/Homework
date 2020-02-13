1b37a692cf40ffcf0839ddbe312c037c6d7bac65
Markus Backman SOFTK20P <markus.backman@centria.fi>
2020-02-13 13:14:59 +0200

    exercise 01 done

--- part1/printing/exercise_01/Program.cs
+++ part1/printing/exercise_01/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace exercise_01
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace exercise_01 {
+    class Program {
+        public static void Main (string[] args) {
             // Add your code here:
-            
+            Console.WriteLine ("Hello World!");
         }
     }
 }

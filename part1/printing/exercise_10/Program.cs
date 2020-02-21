64e10bf81db9f732a4b241c17ce5e5342f31761f
Markus Backman SOFTK20P <markus.backman@centria.fi>
2020-02-21 13:52:12 +0200

    Done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -11,7 +11,7 @@ namespace exercise_10
             string name = Console.ReadLine();
             Console.WriteLine("Give the character a profession:");
             string profession = Console.ReadLine();
-            Console.WriteLine("Here is the story:\n" + "Once upon a time there was a " + profession + " called " + name + "\n" + "On her way to work, " + name + " often pondered what being " + profession + " meant to them." + "\n" , "When you work as a " + profession + " you meet interesting people."  + "\n" + name + " enjoys their work as a " + profession + ",The end.");
+            Console.WriteLine("Here is the story:\n" + "Once upon a time there was a " + profession + " called " + name + "\n" + "On her way to work, " + name + " often pondered what being " + profession + " meant to them.\nWhen you work as a " + profession + " you meet interesting people."  + "\n" + name + " enjoys their work as " + profession + ", The end.");
 
         }
     }

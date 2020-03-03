411f612c5a8f1da276820fdb197310357eac8142
tatz220 <markusbackman@hotmail.com>
2020-03-03 19:23:30 +0200

    done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -7,6 +7,18 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string userinput = Console.ReadLine();
+      int number = Convert.ToInt32(userinput);
+
+      if ((number % 2) == 0)
+      {
+        Console.WriteLine("It is even.");
+      }
+      else
+      {
+        Console.WriteLine("It is odd.");
+      }
 
     }
   }
--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_33
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give the first string:");
+      string userinput = Console.ReadLine();
 
+      Console.WriteLine("Give the second string:");
+      string userinput2 = Console.ReadLine();
+
+      if (userinput == userinput2)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
     }
   }
 }

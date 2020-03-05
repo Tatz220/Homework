b35928ce748f6481f89416430de65cac11e759c9
tatz220 <markusbackman@hotmail.com>
2020-03-05 20:14:22 +0200

    done dotnet crash

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -8,6 +8,16 @@ namespace exercise_42
     {
 
       // Write your code here:
+      string userinput = Console.ReadLine();
+      int number = Convert.ToInt32(userinput);
+
+      string userinput2 = Console.ReadLine();
+      int number2 = Convert.ToInt32(userinput2);
+
+      int sum = number + number2;
+
+      double squareRoot = Math.Sqrt(sum);
+      Console.WriteLine(squareRoot);
     
     }
   }

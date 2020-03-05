aa8a88a0385687e863938286acacccc5cb614668
tatz220 <markusbackman@hotmail.com>
2020-03-05 18:21:07 +0200

    done

--- part1/repetition/exercise_40/Program.cs
+++ part1/repetition/exercise_40/Program.cs
@@ -8,6 +8,29 @@ namespace exercise_40
     {
 
       // Write your code here:
+      int sum = 0;
+      int amount = 0;
+      while(true)
+      {
+        Console.WriteLine("Give a number:");
+        string userinput = Console.ReadLine();
+        int intvalue = Convert.ToInt32(userinput);
+
+        if(intvalue ==0)
+        {
+          break;
+        }
+
+        else
+        {
+          sum = sum + intvalue;
+          amount = amount + 1;
+        }
+      }
+      {
+        Console.WriteLine("Total sum of numbers: " + sum);
+        Console.WriteLine("Total amount of numbers: " + amount);
+      }
     }
   }
 }

726fe2e6a4c458e14993831453f4e530f30ef85b
Markus Backman SOFTK20P <markus.backman@centria.fi>
2020-03-05 11:24:10 +0200

    done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -10,16 +10,21 @@ namespace exercise_37
       // Write your code here:
       while (true)
       {
-      Console.WriteLine("Give a number:");
-      string userinput = Console.ReadLine();
-      int intvalue = Convert.ToInt32(userinput);
+        Console.WriteLine("Give a number:");
+        string userinput = Console.ReadLine();
+        int intvalue = Convert.ToInt32(userinput);
 
-      if (intvalue ==0)
+        if (intvalue ==0)
       {
         break;
       }
-      
-      
+      else
+      {
+      sum = sum +1;
+      }
+      }
+      {
+      Console.WriteLine("Total amount of numbers:" + sum);
       }
     }
   }
--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,7 +7,27 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
+      int sum = 0;
+      while (true)
+      {
+       Console.WriteLine("Give a number:");
+       string userinput = Console.ReadLine();
+       int intvalue = Convert.ToInt32(userinput);
 
+      if (intvalue ==0)
+      {
+        break;
+      }
+      else if (intvalue <0)
+      {
+      sum = sum +1;
+      }
+      else
+      {
+        continue;
+      }
+      }
+      Console.WriteLine("Total amount of negative numbers: " + sum);
     }
   }
 }

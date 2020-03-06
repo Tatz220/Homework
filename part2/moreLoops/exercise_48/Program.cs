6a1740a839d2537debf8bc041d27b4c8315323af
Markus Backman SOFTK20P <markus.backman@centria.fi>
2020-03-06 13:22:17 +0200

    done

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,6 +8,42 @@ namespace exercise_48
     {
 
       // Write your code here:
+      int sum = 0;
+      int number = 0;
+      double average = 0.00;
+      int even = 0;
+      int odd = 0;
+
+      while(true)
+      {
+        Console.WriteLine("Give numbers:");
+        string userinput = Console.ReadLine();
+        int intvalue = Convert.ToInt32(userinput);
+
+        if (intvalue ==-1)
+        {
+          break;
+        }
+        sum = sum + intvalue;
+        number = number +1;
+        average = (double)sum / number;
+
+        if ((intvalue % 2) ==0)
+        {
+          even = even +1;
+        }
+        else
+        {
+          odd =odd +1;
+        }
+
+      }
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Numbers: " + number);
+      Console.WriteLine("Average: " + average);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
  
     }
   }

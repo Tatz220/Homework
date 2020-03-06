3c8afa008959640fd879ef4977c6fef0cb409721
Markus Backman SOFTK20P <markus.backman@centria.fi>
2020-03-06 10:39:39 +0200

    done

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_43
     public static void Main(string[] args)
     {
       // Write your code here:
-      
+      int sum = -1;
+      string userinput = Console.ReadLine();
+      int intvalue = Convert.ToInt32(userinput);
+
+      if (intvalue <0)
+      {
+        Console.WriteLine(intvalue * sum);
+      }
+      else
+      {
+        Console.WriteLine(intvalue);
+      }
     }
   }
 }
--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -8,6 +8,24 @@ namespace exercise_44
     {
 
       // Write your code here:
+      int intvalue = Convert.ToInt32(Console.ReadLine());
+
+      int intvalue2 = Convert.ToInt32(Console.ReadLine());
+
+      if (intvalue > intvalue2)
+      {
+        Console.WriteLine(intvalue + " is greater than " + intvalue2 + ".");
+      }
+      else if (intvalue < intvalue2)
+      {
+        Console.WriteLine(intvalue + " is less than " + intvalue2 + ".");
+      }
+      else
+      {
+        Console.WriteLine(intvalue + " is equal to " + intvalue2 + ".");
+      }
+
+
 
     }
   }
